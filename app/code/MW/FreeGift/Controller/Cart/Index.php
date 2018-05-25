67896788bb432b33b227bb2dc91e0f7e4fb6786d
SyLuu <nguyensiluu@gmail.com>
2018-05-25 18:10:55 +0700

    edit report freegift

---
+++ app/code/MW/FreeGift/Controller/Cart/Index.php
@@ -0,0 +1,99 @@
+<?php
+namespace MW\FreeGift\Controller\Cart;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Checkout\Model\Cart as CustomerCart;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+
+class Index extends \Magento\Checkout\Controller\Cart
+{
+    /**
+     * @var ProductRepositoryInterface
+     */
+    protected $productRepository;
+    protected $helperFreeGift;
+    protected $_ruleFactory;
+    /**
+     * @var \Magento\Framework\View\LayoutFactory
+     */
+    protected $layoutFactory;
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator
+     * @param CustomerCart $cart
+     * @param ProductRepositoryInterface $productRepository
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator,
+        CustomerCart $cart,
+        ProductRepositoryInterface $productRepository,
+        \Magento\Framework\View\LayoutFactory $layoutFactory,
+        \MW\FreeGift\Helper\Data $helperFreeGift,
+        \MW\FreeGift\Model\RuleFactory $ruleFactory
+    ) {
+        parent::__construct(
+            $context,
+            $scopeConfig,
+            $checkoutSession,
+            $storeManager,
+            $formKeyValidator,
+            $cart
+        );
+        $this->layoutFactory = $layoutFactory;
+        $this->productRepository = $productRepository;
+        $this->helperFreeGift = $helperFreeGift;
+        $this->_ruleFactory = $ruleFactory;
+    }
+
+
+
+    /**
+     * Add product to shopping cart action
+     *
+     * @return \Magento\Framework\Controller\Result\Redirect
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     */
+    public function execute()
+    {
+                $cartUrl = $this->_objectManager->get('Magento\Checkout\Helper\Cart')->getCartUrl();
+                $url = $this->_redirect->getRedirectUrl($cartUrl);
+            return $this->goBack($url);
+    }
+
+    /**
+     * Resolve response
+     *
+     * @param string $backUrl
+     * @param \Magento\Catalog\Model\Product $product
+     * @return $this|\Magento\Framework\Controller\Result\Redirect
+     */
+    protected function goBack($backUrl = null, $product = null)
+    {
+        if (!$this->getRequest()->isAjax()) {
+            return parent::_goBack($backUrl);
+        }
+
+        $result = [];
+
+        if ($backUrl || $backUrl = $this->getBackUrl()) {
+            $result['backUrl'] = $backUrl;
+        } else {
+            if ($product && !$product->getIsSalable()) {
+                $result['product'] = [
+                    'statusText' => __('Out of stock')
+                ];
+            }
+        }
+
+        $this->getResponse()->representJson(
+            $this->_objectManager->get('Magento\Framework\Json\Helper\Data')->jsonEncode($result)
+        );
+    }
+
+}
